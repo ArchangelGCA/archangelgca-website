97e266712cdcde8d46ece97c81d432ddc7332adc
ArchangelGCA <archangelgca@gmail.com>
2024-05-25 01:53:02 +0200

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,119 @@
+<script>
+    import Seo from "sk-seo";
+
+    const tooltipConfig = {
+        animation: 'fade',
+        delay: 0,
+        style: {
+            color: 'white',
+            backgroundColor: 'rgba(92,0,166,0.9)',
+            padding: '10px',
+            borderRadius: '5px'
+        },
+        theme: 'text-center w-auto'
+    };
+</script>
+
+<Seo
+        title="ArchangelGCA - Official"
+        description="I specialize in web development, particularly using modern JavaScript frameworks such as Svelte/SvelteKit. When I\'m not coding or studying, I enjoy making digital art."
+        siteName="ArchangelGCA - Official"
+        imageURL="https://avatars.githubusercontent.com/u/159050591?v=4"
+        logo="https://avatars.githubusercontent.com/u/159050591?v=4"
+        author="ArchangelGCA"
+        name="ArchangelGCA"
+        schemaOrg="true"
+        twitter="true"
+        index="true"
+        canonical="https://archangelgca.eu/"
+        keywords="ArchangelGCA, Developer, Artist, Web Development, Svelte, SvelteKit, Digital Art"
+/>
+
+<div class="container my-5">
+    <div class="hero">
+        <h1 class="display-4">Welcome!</h1>
+        <p class="lead">I'm ArchangelGCA, a Developer and Casual Artist.</p>
+        <hr class="my-4">
+        <p>
+            I love to develop projects by day and express my creativity through art by night.
+        </p>
+    </div>
+
+    <div class="about-me text-center mt-4">
+        <h2>About Me</h2>
+        <p>
+            I specialize in web development, particularly using modern JavaScript frameworks such as Svelte/SvelteKit.
+            When I'm not coding or studying, I enjoy making digital art.
+        </p>
+    </div>
+
+    <div class="text-center mt-4">
+        <h2>My Favourite Tools</h2>
+        <div class="row mt-3">
+            <div class="col-md-6 col-lg-3 mb-4">
+                <div class="card card-custom h-100">
+                    <div class="card-body text-center">
+                        <h5 class="card-title">
+                            <i class="fab fa-bootstrap"></i> Bootstrap
+                        </h5>
+                        <p class="card-text">For CSS and more</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-3 mb-4">
+                <div class="card card-custom h-100">
+                    <div class="card-body text-center">
+                        <h5 class="card-title">
+                            <i class="fas fa-database"></i> Supabase
+                        </h5>
+                        <p class="card-text">For all my backend needs.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-3 mb-4">
+                <div class="card card-custom h-100">
+                    <div class="card-body text-center">
+                        <h5 class="card-title">
+                            <i class="fas fa-box"></i> Pocketbase
+                        </h5>
+                        <p class="card-text">Great for simple projects.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-3 mb-4">
+                <div class="card card-custom h-100">
+                    <div class="card-body text-center">
+                        <h5 class="card-title">
+                            <i class="fas fa-code"></i> SvelteKit
+                        </h5>
+                        <p class="card-text">My Framework of choice.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="connect-with-me text-center mt-4">
+        <h2>Connect with Me</h2>
+        <div class="social-links">
+            <a href="https://github.com/ArchangelGCA" target="_blank" rel="noopener" aria-label="GitHub" title="GitHub">
+                <i class="fab fa-github"></i>
+            </a>
+            <a href="https://www.deviantart.com/archangelgca" target="_blank" rel="noopener" aria-label="DeviantArt"
+               title="DeviantArt">
+                <i class="fab fa-deviantart"></i>
+            </a>
+            <a href="https://discord.gg/bp8BNNssEK" target="_blank" rel="noopener" aria-label="Discord" title="Discord">
+                <i class="fab fa-discord"></i>
+            </a>
+            <a href="https://tales.rosesintheflames.com/profile/bfc1c4a7-f2af-494a-b7d9-35377f16d33e" target="_blank"
+               rel="noopener" title="Roses in The Flames"
+               aria-label="RiTF">
+                <i class="fa fa-globe"></i>
+            </a>
+        </div>
+    </div>
+</div>
+
+<style>
+</style>
